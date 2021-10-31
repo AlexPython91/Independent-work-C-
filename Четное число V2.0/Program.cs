17d6b1b9598a801b21630d26fbc347ed5d55289f
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-01 00:37:07 +0400

    Четное число V2.0

---
+++ Четное число V2.0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// Четное ли число с запросом у пользователя
+
+Console.Write("Введите число: ");
+string userAnswer = Console.ReadLine();
+int Answer = Convert.ToInt32(userAnswer);
+
+int a = Answer;
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное!");
+}
+else
+    Console.WriteLine("Число не четное");
