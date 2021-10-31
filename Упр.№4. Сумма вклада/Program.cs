697a092d86a9596c71fe31d5d74e9aa52c037f28
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-01 00:32:18 +0400

    Упр.8 Сумма вклада

---
+++ Упр.№4. Сумма вклада/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// Задача: Ввод суммы вклада. Расчет процентной стаки от суммы вклада пользователя
+
+Console.Write("Введите сумму вклада: ");
+double sum = Convert.ToDouble(Console.ReadLine());
+
+if(sum < 100) 
+{
+    sum += sum * 0.05;
+}
+else if(sum <= 200)
+{
+    sum += sum * 0.07;
+}
+else
+{
+    sum += sum * 0.1;
+}
+Console.WriteLine("Сумма вашего вклада с учетом процентов: " + sum);
+
