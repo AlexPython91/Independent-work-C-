3823bf4eb3efb9a020e0402c1d4c7fd58644bd91
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-01 00:21:53 +0400

    Упр.2 Из градусов в Фаренгейты

---
+++ Из градусов в Фаренгейты/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+// Перевдо из С в F
+Console.Write("Сколько градусов цельсия Вы хотите преобразовать? ");
+
+    string userAnswer = Console.ReadLine();
+    int degrees = Convert.ToInt32(userAnswer);
+    int c = degrees;
+
+double f = degrees*1.8+32;
+Console.WriteLine("Температура по Фаренгейту = " + f);
