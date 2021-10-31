4bfc207cde5cbe28f3ee664a103fcc2763934fe6
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-01 00:23:56 +0400

    max из 2х чисел

---
+++ Наибольшее из 2 чисел/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+int numberOne = 14;
+int numberTwo = 10;
+int max = numberOne;
+
+if (numberOne > numberTwo) max = numberOne;
+else
+{
+    max = numberTwo;
+}
+Console.WriteLine(max);
