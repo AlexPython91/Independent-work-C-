5f8e03ad5c5f5e72ccae5dc93b740e027c7d2c12
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-01 00:31:06 +0400

    Упр.7 1 число (либо/равно)

---
+++ Упр.№3. 1 число. Либо равно/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// Задача: Ввод 1го числа, программа вычисляет (одно число "либо" другое)
+
+Console.Write("Введите одно число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number == 5 | number == 10)
+{
+    Console.WriteLine("Число " + number + " либо равно 5, либо равно 10");
+}
+else
+{
+    Console.WriteLine("Неизвестное число");
+}
