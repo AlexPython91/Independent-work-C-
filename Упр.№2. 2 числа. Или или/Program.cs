e733cdac9e7aea020b4f1df22122eee931253e7d
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-01 00:29:32 +0400

    Упр6. 2 числа (или/или)

---
+++ Упр.№2. 2 числа. Или или/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// Задача: Ввод 2х чисел. Вычисление чисел (больше и меньше)
+
+Console.Write("Введите одно число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 5 && number < 10)
+{
+    Console.WriteLine("Число больше 5 и меньше 10");
+}
+else
+{
+    Console.WriteLine("Неизвестное число");
+}
